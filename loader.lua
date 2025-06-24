75fe4f2eba4f66096189323f83d36fa625a7a507
DRZ1NK <creepyman12212@gmail.com>
2025-06-25 01:13:32 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local gameId = game.PlaceId
-if gameId == 4889315193 then
+if gameId == 4939362930 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/trdga/refs/heads/main/trdmain.lua"))()
 elseif gameId == 15101393044 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB"))()

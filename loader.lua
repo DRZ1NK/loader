242ab316c2339d4b91bc33bc88c5bc3e48f77866
DRZ1NK <creepyman12212@gmail.com>
2025-06-27 01:01:03 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,6 +1,10 @@
 local gameId = game.PlaceId
 if gameId == 4939362930 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/trdga/refs/heads/main/trdmain.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua"))()
 elseif gameId == 15101393044 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua"))()
+elseif gameId == 18260801046 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua"))()
+elseif gameId = 100759070542523 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GSH.lua"))()
 end

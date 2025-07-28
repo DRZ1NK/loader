cb69c0257fcc7a473814c5ff8a9cc97ae5ea8821
DRZ1NK <creepyman12212@gmail.com>
2025-07-28 17:33:10 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -5,8 +5,10 @@ elseif gameId == 15101393044 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua"))()
 elseif gameId == 18260801046 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua"))()
-elseif gameId = 100759070542523 then
+elseif gameId == 100759070542523 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GSH.lua"))()
-elseif gameId = 86363491117206 then
+elseif gameId == 86363491117206 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua"))()
+elseif gameId == 114460042966606 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/SAK.lua"))()
 end

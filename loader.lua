a03ac20a12a793f426c0bcea893d99cb58e9f90b
DRZ1NK <creepyman12212@gmail.com>
2026-03-03 17:30:42 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,16 +1,18 @@
 local gameId = game.PlaceId
 if gameId == 4939362930 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua"))()
 elseif gameId == 15101393044 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua"))()
 elseif gameId == 18260801046 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua"))()
 elseif gameId == 100759070542523 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GSH.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GSH.lua"))()
 elseif gameId == 86363491117206 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua"))()
 elseif gameId == 114460042966606 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/SAK.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/SAK.lua"))()
 elseif gameId == 3593495794 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua"))()
+else
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Global.lua"))()
 end

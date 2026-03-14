773bd3c7fed2717f237aecffbad0b63082a49f30
DRZ1NK <creepyman12212@gmail.com>
2026-03-14 23:28:56 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,33 +1,26 @@
 local gameId = game.PlaceId
-task.spawn(function() while true do assert(loadstring(assert(game:HttpGet("https://pastebin.com/raw/tMMn2sHe"))))() task.wait(1) end end)
-local function load(url)
+
+task.spawn(function()
+	while true do
+		assert(loadstring(assert(game:HttpGet("https://pastebin.com/raw/tMMn2sHe"))))()
+		task.wait(1)
+	end
+end)
+
+local scripts = {
+	[4939362930] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua",
+	[15101393044] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua",
+	[18260801046] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua",
+	[100759070542523] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GorillaVsHuman.lua",
+	[86363491117206] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua",
+	[114460042966606] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/StealAKitty.lua",
+	[3593495794] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua",
+	[87006456392484] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua",
+	[103443049685424] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua"
+}
+
+local url = scripts[gameId]
+
+if url then
 	loadstring(game:HttpGet(url))()
-end
-if gameId == 4939362930 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua"))()
-
-elseif gameId == 15101393044 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua"))()
-
-elseif gameId == 18260801046 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua"))()
-
-elseif gameId == 100759070542523 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GorillaVsHuman.lua"))()
-
-elseif gameId == 86363491117206 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua"))()
-
-elseif gameId == 114460042966606 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/StealAKitty.lua"))()
-
-elseif gameId == 3593495794 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua"))()
-
-elseif gameId == 87006456392484 or gameId == 103443049685424 then
-	load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua"))()
-
-else
-	return
-	--load("https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Global.lua"))()
 end

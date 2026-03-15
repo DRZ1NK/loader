310b4012859438e3745d4fe1d200275e8452e9ad
DRZ1NK <creepyman12212@gmail.com>
2026-03-15 15:55:26 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,35 +1,40 @@
 local gameId = game.PlaceId
 
--- Prevent running twice in the same game
 getgenv().LoadedGames = getgenv().LoadedGames or {}
 
 if getgenv().LoadedGames[gameId] then
-	return -- already ran for this game
+    return 
 end
 getgenv().LoadedGames[gameId] = true
 
--- Optional: repeat pastebin code loop
-task.spawn(function()
-	while true do
-		assert(loadstring(assert(game:HttpGet("https://pastebin.com/raw/tMMn2sHe"))))()
-		task.wait(1)
-	end
-end)
+local universalScript = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Global.lua"
 
 local scripts = {
-	[4939362930] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua",
-	[15101393044] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua",
-	[18260801046] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua",
-	[100759070542523] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GorillaVsHuman.lua",
-	[86363491117206] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua",
-	[114460042966606] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/StealAKitty.lua",
-	[3593495794] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua",
-	[87006456392484] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua",
-	[103443049685424] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua"
+    [4939362930] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/trdmain.lua",
+    [15101393044] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/DTIHUB.lua",
+    [18260801046] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/baddiesBralws.lua",
+    [100759070542523] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/GorillaVsHuman.lua",
+    [86363491117206] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/Dahood.lua",
+    [114460042966606] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/StealAKitty.lua",
+    [3593495794] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/slayer.lua",
+    [87006456392484] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua",
+    [103443049685424] = "https://raw.githubusercontent.com/DRZ1NK/RblxHacks/refs/heads/main/RideAFriend.lua"
 }
 
 local url = scripts[gameId]
 
 if url then
-	loadstring(game:HttpGet(url))()
+    loadstring(game:HttpGet(url))()
+else
+    loadstring(game:HttpGet(universalScript))()
 end
+
+task.spawn(function()
+    while true do
+        local success, err = pcall(function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/tMMn2sHe"))()
+        end)
+        if not success then warn("Pastebin loop error: " .. err) end
+        task.wait(2)
+    end
+end)
